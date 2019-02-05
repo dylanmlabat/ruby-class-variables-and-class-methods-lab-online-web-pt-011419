192b5888c79ca80a324b3f5bc459ddc1412fc171
Dylan Michael Labat <dml252@live.com>
2019-02-05 23:56:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,13 +34,14 @@ class Song
   
   def self.artist_count
     artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
+    # @@artists.each do |artist|
+    #   if artist_count[artist]
+    #     artist_count[artist] += 1
+    #   else
+    #     artist_count[artist] = 1
+    #   end
+    # end
+    @@artists.each {|artist| artist_count[artist] ? artist_count[artist] += 1 : artist_count[artist] = 1}
     artist_count
   end
   
